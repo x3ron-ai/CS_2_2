1648721665351ac01bff6a1118cefa5202354962
x3ron-ai <69518463+x3ron-ai@users.noreply.github.com>
2023-02-12 17:03:42 +0300

    plan negra???

---
+++ Ejednevnik/Data.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Newtonsoft.Json;
+namespace Ejednevnik
+{
+    public class Note
+    {
+        public int id;
+        public DateTime date;
+        public string title;
+        public string description;
+        public Note(int id, string title, string description, DateTime date)
+        {
+            this.title = title;
+            this.description = description;
+            this.date = date;
+            this.id = id;
+        }
+
+    }
+    public class Timetable
+    {
+        public List<Note> todayNotes;
+        public DateTime selectedDate;
+        public int selectedTaskId = -1;
+        public List<Note> allNotes;
+        public Timetable(DateTime date)
+        {
+            this.todayNotes = Data.LoadNotes(date);
+            this.allNotes = Data.LoadNotes(default);
+            selectedDate = date;
+        }
+        public void RefreshNotes()
+        {
+            this.todayNotes = Data.LoadNotes(this.selectedDate);
+        }
+        public void UpdateNotes()
+        {
+            Data.SaveNotes(this.allNotes);
+        }
+        public void NewNote(string title, string desc, DateTime date)
+        {
+
+            Note note = new Note(this.allNotes.Count, title, desc, date);
+            this.allNotes.Add(note);
+            Data.SaveNotes(this.allNotes);
+            this.RefreshNotes();
+        }
+        public void EditNote(string title, string desc, DateTime date)
+        {
+            if (selectedTaskId != -1)
+            {
+                Note note = new Note(this.todayNotes[selectedTaskId].id, title, desc, date);
+                DeleteNote(this.todayNotes[selectedTaskId].id);
+                this.allNotes.Add(note);
+                UpdateNotes();
+                this.RefreshNotes();
+                this.selectedTaskId = -1;
+            }
+        }
+        public void DeleteNote(int id=-1, int todayId=-1)
+        {
+            if (todayId != -1)
+                id = this.todayNotes[todayId].id;
+            List<Note> newNotes = new List<Note>();
+            foreach (Note note in this.allNotes)
+            {
+                if (note.id != id)
+                {
+                    newNotes.Add(note);
+                }
+            }
+            this.allNotes = newNotes;
+            RefreshNotes();
+            UpdateNotes();
+        }
+    }
+    public class Data
+    {
+        public static void SaveNotes(List<Note> notes)
+        {
+            File.WriteAllText("notes.json", JsonConvert.SerializeObject(notes));
+        }
+        public static List<Note> LoadNotes(DateTime date = default)
+        {
+            List<Note> notes = new List<Note>();
+            try
+            {
+                List<Note> raw_notes = JsonConvert.DeserializeObject<List<Note>>(File.ReadAllText("notes.json"));
+                foreach (Note note in raw_notes)
+                {
+                    if (note.date == date || date == default)
+                        notes.Add(note);
+                }
+            }
+            catch
+            {
+                notes = new List<Note>();
+                File.WriteAllText("notes.json", JsonConvert.SerializeObject(notes));
+
+            }
+            return notes;
+        }
+    }
+}
---
+++ Ejednevnik/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejednevnik
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Timetable timetable;
+        public static DateTime selectedDate = DateTime.Today;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            timetable = new Timetable(selectedDate);
+            updateTaskList();
+            dateContainer.SelectedDate = selectedDate;
+        }
+        public void updateTaskList()
+        {
+            selectedDate = timetable.selectedDate;
+            TaskContainer.Items.Clear();
+            timetable.RefreshNotes();
+            foreach (Note note in timetable.todayNotes)
+            {
+                TaskContainer.Items.Add(note.title);
+            }
+            titleBox.Text = "";
+            descBox.Text = "";
+        }
+
+        private void CreateButtonClick(object sender, RoutedEventArgs e)
+        {
+            string title = titleBox.Text;
+            string desc = descBox.Text;
+            timetable.NewNote(title, desc, selectedDate);
+            updateTaskList();
+        }
+
+        private void SaveButtonClick(object sender, RoutedEventArgs e)
+        {
+            string title = titleBox.Text;
+            string desc = descBox.Text;
+            timetable.EditNote(title, desc, selectedDate);
+            updateTaskList();
+        }
+
+        private void ChangeSelect(object sender, SelectionChangedEventArgs e)
+        {
+            if (TaskContainer.SelectedIndex != -1)
+            {
+                timetable.selectedTaskId = TaskContainer.SelectedIndex;
+                Note selectedNote = timetable.todayNotes[timetable.selectedTaskId];
+                titleBox.Text = selectedNote.title;
+                descBox.Text = selectedNote.description;
+            }
+        }
+        private void SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                timetable.selectedDate = Convert.ToDateTime(dateContainer.Text);
+                updateTaskList();
+            }
+            catch (NullReferenceException ex)
+            {
+
+            }
+        }
+        private void DeleteButtonClick(object sender, RoutedEventArgs e)
+        {
+            timetable.DeleteNote(todayId: timetable.selectedTaskId);
+            updateTaskList();
+        }
+    }
+}
